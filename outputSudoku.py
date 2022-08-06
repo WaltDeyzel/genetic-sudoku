b01bd0d8c1ec8cca167f53f64822d19fae00022a
Walt Deyzel <waltdeyzel3141@gmail.com>
2022-08-06 19:40:48 +0200

    Implement find error in grid: Show error in image in red

--- outputSudoku.py
+++ outputSudoku.py
@@ -1,5 +1,5 @@
 from cv2 import cv2
-
+from generate import checkForErrorInGrid
 def filterDigits(digits):
     numbers = [ '1', '2', '3', '4', '5', '6', '7', '8', '9']
     filtered_digits = []
@@ -22,34 +22,66 @@ def getImg(digits, number):
     return -1
 
 
-def image_output(image, gird, digits):
+def image_output(image, grid, digits):
 
     image = image.copy()
     width = len(image)//9
-
+    ofsety = int(width / 3)
+    ofsetx = int(width / 6)
+    errorNumbersXY = checkForErrorInGrid(grid)
+    
     for row in range(9):
         for col in range(9):
-            number = int(gird[row, col])
+            number = int(grid[row, col])
             y = int(row*width)
             x = int(col*width)
             num = getImg(filterDigits(digits), number)
             if num == -1:
-                cv2.putText(image, str(number), (x, y), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0))
+                cv2.putText(image, str(number), (x + ofsetx, y + ofsety), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0))
             else:
                 cut = 10
                 m = len(num.getNumImg()) - cut+3
                 image[y+cut:y+m, x+cut:x+m] = num.getNumImg()[cut:m, +cut:m]
-                
-                #     cv2.imshow('shapes', image[x:x+m, y:y+m])
-                #     cv2.waitKey(0)
-                #     cv2.destroyAllWindows()
-            #cv2.putText(img, str(number), (x, y), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0))
+            
+            # TOD0: Create toggle 
+            if True:
+                for cor in errorNumbersXY:
+                    # print('Check4Error implementation')
+                    x = int(cor[0]*width)
+                    y = int(cor[1]*width)
+                    startPoint = (x,  y)
+                    endPoint = (x + width, y + width)
+                    # print(startPoint, endPoint)
+                    cv2.rectangle(image, startPoint, endPoint, (0, 0, 255), 3)
     
     cv2.imshow('shapes', image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
             
+if __name__ == '__main__':
+    import numpy as np
+    from inputSudoku import image_input
+    problem_grid = np.array([
+    [1,0,0, 0,0,0, 0,0,0],
+    [0,2,0, 0,0,0, 0,0,0],
+    [0,0,0, 0,3,0, 0,3,0],
 
+    [0,0,0, 0,0,0, 0,0,0],
+    [0,2,0, 0,0,0, 0,0,0],
+    [0,0,0, 0,4,4, 4,0,0],
 
+    [1,0,0, 0,0,0, 0,0,0],
+    [0,2,0, 0,0,0, 0,0,0],
+    [0,0,0, 0,0,0, 0,0,0],
+    ])
+    # Name of puzzle image 
+    # Note image does not correspond to input grid
+    img = 'images/p2.jpg'  
+    # Locate Sudoku grid and return only the grid
+    puzzle_img = image_input(img)
+    cv2.imshow('shapes', puzzle_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    image_output(puzzle_img, problem_grid, [])
 
 
