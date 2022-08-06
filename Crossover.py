6636ac767c8c68fb535cca076047ba57315cc985
Walt Deyzel <waltdeyzel3141@gmail.com>
2022-08-06 23:20:34 +0200

    Pre allocate array

--- Crossover.py
+++ Crossover.py
@@ -1,6 +1,8 @@
 from numpy import random as npR
+import numpy as np
 
 def crossover(option_1, option_2, option_3):
+    # This function joins two strands of DNA to make a new one.
 
     dna_1 = option_1.getDNA()
     dna_2 = option_2.getDNA()
@@ -12,8 +14,9 @@ def crossover(option_1, option_2, option_3):
 
 def row(dna_1, dna_2, dna_3):
 
-    child = dna_1.copy()
+    child = np.zeros((9,9))
     
+    # Copy rows [0,3], [3,6], [6,9] from different DNA to child
     child[0:3, :] = dna_1[0:3, :]
     child[3:6, :] = dna_2[3:6, :]
     child[6:9, :] = dna_3[6:9, :]
@@ -22,8 +25,9 @@ def row(dna_1, dna_2, dna_3):
 
 def col(dna_1, dna_2, dna_3):
 
-    child = dna_3.copy()
-    
+    child = np.zeros((9,9))
+
+    # Copy columns [0,3], [3,6], [6,9] from different DNA to child
     child[:, 0:3] = dna_1[:, 0:3]
     child[:, 3:6] = dna_2[:, 3:6]
     child[:, 6:9] = dna_3[:, 6:9]
